022b8d9f8e270bb75a879b9b176e5c24c45ac44b
zyyxp-linhnh <linhnh@zyyx.jp>
2024-12-05 16:40:53 +0700

    make animation close draggable bottom sheet

--- lib/screens/map/map_controller.dart
+++ lib/screens/map/map_controller.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapController extends GetxController {
 
   final mapCompleter = Completer<GoogleMapController>();
-  final currentExtent = 0.3.obs;
+  // Khởi tạo DraggableScrollableController
+  final DraggableScrollableController draggableScrollableController = DraggableScrollableController();
+  final currentExtent = 0.2.obs;
 
   final CameraPosition kGooglePlex = const CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -22,5 +25,14 @@ class MapController extends GetxController {
   Future<void> goToTheLake() async {
     final GoogleMapController controller = await mapCompleter.future;
     await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
+  // Phương thức thu gọn sheet
+  void collapseSheet() {
+    draggableScrollableController.animateTo(
+      0.2, // giá trị minChildSize
+      duration: const Duration(milliseconds: 350), // Thời gian hoạt động
+      curve: Curves.easeInOut, // Đường cong chuyển động
+    );
   }
 }
--- lib/screens/map/map_page.dart
+++ lib/screens/map/map_page.dart
@@ -38,6 +38,7 @@ class MapPage extends StatelessWidget {
             },
           ),
           DraggableScrollableSheet(
+              controller: controller.draggableScrollableController,
               initialChildSize: controller.currentExtent.value,
               minChildSize: 0.2,
               maxChildSize: 1,
@@ -49,7 +50,8 @@ class MapPage extends StatelessWidget {
                     slivers: [
                       // SliverPersistentHeader
                       SliverPersistentHeader(
-                        delegate: MySliverHeaderDelegate(),
+                        delegate: MySliverHeaderDelegate(
+                            onTabClose: () => controller.collapseSheet()),
                         pinned: true,
                       ),
                       SliverList(
@@ -82,30 +84,41 @@ class MySliverHeaderDelegate extends SliverPersistentHeaderDelegate {
   @override
   double get maxExtent => 50.0; // Chiều cao tối đa khi không cuộn
 
+  final VoidCallback onTabClose;
+
+  MySliverHeaderDelegate({required this.onTabClose});
+
   @override
   Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
+    return Stack(
       alignment: Alignment.center,
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 15),
-      decoration: const BoxDecoration(
-        color: ColorName.white,
-        border: Border(
-          bottom: BorderSide(
-            color: ColorName.greyWhite,
-            width: 0.5,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            color: ColorName.white,
+            border: Border(
+              bottom: BorderSide(
+                color: ColorName.greyWhite,
+                width: 0.5,
+              ),
+            ),
+          ),
+          child: Text(
+            "Danh sách địa điểm",
+            style: text16bold,
           ),
         ),
-      ),
-      child: Text(
-        "Danh sách địa điểm",
-        style: text16bold,
-      ),
+        Positioned(right: 15, child: InkWell(
+          onTap: () => onTabClose.call(),
+            child: const Icon(Icons.close, size: 25, color: ColorName.black,))),
+      ],
     );
   }
 
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return false; // Không cần phải xây dựng lại khi có sự thay đổi
+    return false;
   }
 }
